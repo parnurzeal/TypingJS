236e8b3ca605640ff1956898edbcb0f86c45b7c2
Theeraphol Wattanavekin <parnurzeal@gmail.com>
2013-10-30 17:42:36 +0900

    made text moving down

--- js/action/typing-game.js
+++ js/action/typing-game.js
@@ -6,20 +6,40 @@ $(document).ready(function(){
     for(var i = 0;i<word_list.length;i++){
         $('.game-body').append('<div class="word" id="word'+i+'">'+word_list[i].word+'</div>');
     }
+    initial();
 });
 
-function move(){
-    var all_div = $('.game-body').children();
-    console.log(all_div);
+function findPos(element){
+    if(element){
+        var parentPos = findPos(element.offsetParent);
+        return {
+            x:parentPos.x + element.offsetLeft,
+            y:parentPos.y + element.offsetTop
+        };
+    }else{
+        return {x:0,y:0};
+    }
+}
+function initial(){
+    var all_div=$('.game-body').children();
     for(var i = 0;i<all_div.length;i++){
-        console.log(all_div[i].id);
-        var current_top = $('#'+all_div[i].id).css('top');
-        console.log(current_top);
-        //all_div[i].css('top',current_top+10+'px');
+        // TODO: change 1000 to max width of canvas or windows
+        var rand_left=Math.floor(Math.random()*1000);
+        $('#'+all_div[i].id).css('left',rand_left+'px');
     }
 }
 
+function move(){
+    var all_div = $('.game-body').children();
+    for(var i = 0;i<all_div.length;i++){
+        var curPos = findPos(all_div[i]);
+        // TODO: change 10 to speed
+        var newPos = {x:curPos.x,y:curPos.y+10}
+        $('#'+all_div[i].id).css('top',newPos.y+'px');
+    }
+}
 
+// TODO: change 1000 to appropriate small number
 var timer = Math.floor(Math.random() +1000);
 window.setInterval(function(){
     move();
